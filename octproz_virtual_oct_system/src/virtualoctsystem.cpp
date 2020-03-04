abf222c299807ada24b7800658973cacd141bfba
spectralcode <51879191+spectralcode@users.noreply.github.com>
2020-03-04 15:14:03 +0100

    Initial commit

---
+++ octproz_virtual_oct_system/src/virtualoctsystem.cpp
@@ -0,0 +1,249 @@
+/*
+MIT License
+
+Copyright (c) 2019-2020 Miroslav Zabic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "virtualoctsystem.h"
+
+VirtualOCTSystem::VirtualOCTSystem() {
+	this->setType((PLUGIN_TYPE)SYSTEM);
+	this->systemDialog = new VirtualOCTSystemSettingsDialog();
+	this->settingsDialog = static_cast<QDialog*>(this->systemDialog);
+	this->name = "Virtual OCT System";
+	this->file = nullptr;
+	this->multiBuffer = nullptr;
+
+	connect(this->systemDialog, &VirtualOCTSystemSettingsDialog::settingsUpdated, this, &VirtualOCTSystem::slot_updateParams);
+	connect(this, &VirtualOCTSystem::enableGui, this->systemDialog, &VirtualOCTSystemSettingsDialog::slot_enableGui);
+}
+
+VirtualOCTSystem::~VirtualOCTSystem() {
+	this->cleanup();
+	qDebug() << "VirtualOCTSystem destructor. Thread ID: " << QThread::currentThreadId();
+}
+
+bool VirtualOCTSystem::init() {
+	//open file
+	QString fileName;
+	if (this->currParams.filePath.size() < 2) {
+		emit error(tr("No file selected for virtual OCT system."));
+		return false;
+	}else{
+		fileName = this->currParams.filePath;
+	}
+	this->file = fopen(fileName.toLatin1(), "r");
+	if (file == nullptr) {
+		emit error(tr("Unable to open file for virtual OCT system!"));
+		return false;
+	}
+
+	//allocate buffer memory
+	size_t bufferSize = currParams.width*currParams.height*currParams.depth*ceil((double)this->currParams.bitDepth / 8.0);
+	this->buffer->allocateMemory(2, bufferSize);
+
+	//create additional buffers if user wants to read multiple buffers per file
+	if(currParams.buffersFromFile > 2){
+		this->multiBuffer = new AcquisitionBuffer();
+		this->multiBuffer->allocateMemory(currParams.buffersFromFile, bufferSize);
+	}
+
+	//emit info signal for OCTproZ message log console
+	emit info (tr("Virtual OCT system initialized!"));
+	
+	//init was successful
+	return true;
+}
+
+
+void VirtualOCTSystem::startAcquisition(){
+	//init acquisition
+	bool initSuccessfull = this->init();
+	if(!initSuccessfull){
+		emit enableGui(true);
+		emit info(tr("Initialization unsuccessful. Acquisition stopped."));
+		this->buffer->releaseMemory();
+		this->cleanup();
+		emit acquisitionStopped();
+		return;
+	}
+
+	//start acquisition
+	emit info("Acquisition started");
+	if(currParams.buffersFromFile <= 2){
+		this->acqcuisitionSimulation();
+	}else{
+		this->acquisitionSimulationWithMultiFileBuffers();
+	}
+
+	//acuquisition stopped
+	emit enableGui(true);
+	emit info("Acquisistion stopped!");
+	emit acquisitionStopped();
+	//wait some time before releasing buffer memory to allow extensions and 1d plot window to process last raw buffer
+	QCoreApplication::processEvents();
+	QThread::msleep(500);
+	QCoreApplication::processEvents();
+	this->buffer->releaseMemory();
+	this->cleanup();
+}
+
+void VirtualOCTSystem::stopAcquisition(){
+	this->acqusitionRunning = false;
+	qDebug() << "Plugin Thread ID stopAcq: " << QThread::currentThreadId();
+}
+
+void VirtualOCTSystem::cleanup() {
+	if(this->multiBuffer != nullptr){
+		delete this->multiBuffer;
+		this->multiBuffer = nullptr;
+	}
+}
+
+void VirtualOCTSystem::settingsLoaded(QVariantMap settings){
+	this->systemDialog->setSettings(settings);
+}
+
+void VirtualOCTSystem::acqcuisitionSimulation(){
+	//calculate size of buffer
+	uint numberOfElements = this->currParams.depth * currParams.width * currParams.height;
+	uint sizeOfElement = ceil((double)this->currParams.bitDepth / 8.0);
+
+	//read data from file into first buffer
+	void* buf = static_cast<void*>(this->buffer->bufferArray[0]);
+	fread(buf, sizeOfElement, numberOfElements, this->file);
+
+	//set position indicater associated with this->file
+	if(currParams.buffersFromFile == 2){
+		fseek(this->file, numberOfElements*sizeOfElement, SEEK_SET);
+	}else{
+		rewind(this->file);
+	}
+
+	//read data from file into second buffer
+	buf = static_cast<void*>(this->buffer->bufferArray[1]);
+	fread(buf, sizeOfElement, numberOfElements, this->file);
+
+	//close file
+	fclose(this->file);
+	qDebug() << "file closed";
+
+	//acquisition begins!
+	emit enableGui(false);
+	this->acqusitionRunning = true;
+	this->buffer->currIndex = 1;
+	emit acquisitionStarted(this);
+	while (this->acqusitionRunning) {
+
+		//wait untill processing thread is done with previous acquisition buffer. This is not necessary in real oct systems and may even slow down the overall processing speed. In real oct systems just check the bufferReadyArray flag of next buffer.
+		while(this->buffer->bufferReadyArray[buffer->currIndex] == true){
+			QThread::usleep(100);
+			QCoreApplication::processEvents();
+		}
+
+		//calculate index of next buffer
+		int nextIndex = (this->buffer->currIndex+1)%2;
+
+		//set acquisition buffer index, so that processing thread knows current buffer
+		this->buffer->currIndex = nextIndex;
+
+		//check bufferReadyArray flag to see if acquisition system is allowed to reuse this buffer
+		if(buffer->bufferReadyArray[nextIndex] == false){
+
+			//actual data acquisition could be placed here. the content of this->buffer->bufferArray[nextIndex] could be modified here, but the acquisition buffer already contains the desired data so we just set the bufferReadyArray to true
+			//set bufferReadyArray to true to allow processing of buffer
+			this->buffer->bufferReadyArray[nextIndex] = true;
+
+		}
+		//user defined wait time
+		QThread::usleep((this->currParams.waitTimeUs));
+	}
+}
+
+void VirtualOCTSystem::acquisitionSimulationWithMultiFileBuffers() {
+	//calculate size of buffer
+	uint numberOfElements = this->currParams.depth * currParams.width * currParams.height;
+	uint sizeOfElement = ceil((double)this->currParams.bitDepth / 8.0);
+	size_t bufferSizeInBytes = numberOfElements*sizeOfElement;
+
+	//read data from file into file buffers
+	for(int i = 0; i < currParams.buffersFromFile; i++){
+		void* buf = static_cast<void*>(this->multiBuffer->bufferArray[i]);
+		fseek(this->file, i*numberOfElements*sizeOfElement, SEEK_SET);
+		fread(buf, sizeOfElement, numberOfElements, this->file);
+	}
+
+	//close file
+	fclose(this->file);
+	qDebug() << "file closed";
+
+	//acquisition begins!
+	emit enableGui(false);
+	this->acqusitionRunning = true;
+	this->buffer->currIndex = 0;
+	int nextIndex = 1;
+	int multiBufferIndex = currParams.buffersFromFile-1;
+	emit acquisitionStarted(this);
+	while (this->acqusitionRunning) {
+		//wait untill processing thread is done with previous acquisition buffer. This is not necessary in real oct systems and may even slow down the overall processing speed. In real oct systems just check the bufferReadyArray flag of the next buffer.
+		while(this->buffer->bufferReadyArray[buffer->currIndex] == true){
+			QThread::usleep(100);
+			QCoreApplication::processEvents();
+		}
+
+		//set acquisition buffer index, so that processing thread knows current buffer
+		this->buffer->currIndex = nextIndex;
+
+		//check bufferReadyArray flag to see if acquisition system is allowed to reuse this buffer and write new data in acquisition buffer
+		if(this->buffer->bufferReadyArray[nextIndex] == false){
+			//get current buffer positions
+			multiBufferIndex = (multiBufferIndex+1)%currParams.buffersFromFile;
+			void* currAcquisitionBuf = static_cast<void*>(this->buffer->bufferArray[nextIndex]);
+			void* currMultiBuf = static_cast<void*>(this->multiBuffer->bufferArray[multiBufferIndex]);
+
+			//copy data from multiBuffer to acquisitionBuffer
+			memcpy(currAcquisitionBuf, currMultiBuf, bufferSizeInBytes);
+
+			//set bufferReadyArray to true to allow processing of buffer
+			this->buffer->bufferReadyArray[nextIndex] = true;
+
+			//calculate index of next buffer
+			nextIndex = (this->buffer->currIndex+1)%2;
+		}
+		//user defined wait time
+		QThread::usleep((this->currParams.waitTimeUs));
+	}
+}
+
+void VirtualOCTSystem::slot_updateParams(simulatorParams newParams){
+	this->currParams = newParams;
+	AcquisitionParams params;
+	params.samplesPerLine = newParams.width;
+	params.ascansPerBscan = newParams.height;
+	params.bscansPerBuffer = newParams.depth;
+	params.buffersPerVolume = newParams.buffersPerVolume;
+	params.bitDepth = newParams.bitDepth;
+	this->params->slot_updateParams(params);
+
+	//store settings, so settings can be reloaded into gui at next start of application
+	this->systemDialog->getSettings(&this->settingsMap);
+	emit storeSettings(this->name, this->settingsMap);
+}
---
+++ octproz_virtual_oct_system/src/virtualoctsystemsettingsdialog.cpp
@@ -0,0 +1,107 @@
+/*
+MIT License
+
+Copyright (c) 2019-2020 Miroslav Zabic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "virtualoctsystemsettingsdialog.h"
+#include "virtualoctsystem.h"
+
+VirtualOCTSystemSettingsDialog::VirtualOCTSystemSettingsDialog(QWidget *parent)
+	: ui(new Ui::VirtualOCTSystemSettingsDialog) //QDialog(parent)
+{
+	ui->setupUi(this);
+	initGui();
+
+	///qRegisterMetaType is needed to enabel Qt::QueuedConnection for signal slot communication with "simulatorParams"
+	qRegisterMetaType<simulatorParams >("simulatorParams");
+}
+
+VirtualOCTSystemSettingsDialog::~VirtualOCTSystemSettingsDialog()
+{
+}
+
+void VirtualOCTSystemSettingsDialog::setSettings(QVariantMap settings){
+	this->ui->lineEdit->setText(settings.value(FILEPATH).toString());
+	this->ui->spinBox_bitDepth->setValue(settings.value(BITDEPTH).toInt());
+	this->ui->spinBox_width->setValue(settings.value(WIDTH).toInt());
+	this->ui->spinBox_height->setValue(settings.value(HEIGHT).toInt());
+	this->ui->spinBox_depth->setValue(settings.value(DEPTH).toInt());
+	this->ui->spinBox_buffersPerVolume->setValue(settings.value(BUFFERS_PER_VOLUME).toInt());
+	this->ui->spinBox_buffersFromFile->setValue(settings.value(BUFFERS_FROM_FILE).toInt());
+	this->ui->spinBox_waitTime->setValue(settings.value(WAITTIME).toInt());
+	this->slot_apply();
+}
+
+void VirtualOCTSystemSettingsDialog::getSettings(QVariantMap* settings) {
+	settings->insert(FILEPATH, this->ui->lineEdit->text());
+	settings->insert(BITDEPTH, this->ui->spinBox_bitDepth->value());
+	settings->insert(WIDTH, this->ui->spinBox_width->value());
+	settings->insert(HEIGHT, this->ui->spinBox_height->value());
+	settings->insert(DEPTH, this->ui->spinBox_depth->value());
+	settings->insert(BUFFERS_PER_VOLUME, this->ui->spinBox_buffersPerVolume->value());
+	settings->insert(BUFFERS_FROM_FILE, this->ui->spinBox_buffersFromFile->value());
+	settings->insert(WAITTIME, this->ui->spinBox_waitTime->value());
+}
+
+void VirtualOCTSystemSettingsDialog::initGui(){
+	this->setWindowTitle(tr("Virtual OCT System Settings"));
+	connect(this->ui->pushButton_selectFile, &QPushButton::clicked, this, &VirtualOCTSystemSettingsDialog::slot_selectFile);
+	connect(this->ui->okButton, &QPushButton::clicked, this, &VirtualOCTSystemSettingsDialog::slot_apply);
+	connect(this->ui->spinBox_width, &QSpinBox::editingFinished, this, &VirtualOCTSystemSettingsDialog::slot_checkWidthValue);
+}
+
+void VirtualOCTSystemSettingsDialog::slot_selectFile(){
+	QString standardLocation = this->params.filePath.size() == 0 ? QStandardPaths::writableLocation(QStandardPaths::DesktopLocation) : this->params.filePath;
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Raw OCT Volume "), standardLocation, tr("Raw OCT Volume File (*.raw)"));
+	this->ui->lineEdit->setText(fileName);
+}
+
+void VirtualOCTSystemSettingsDialog::slot_apply() {
+	this->params.filePath = this->ui->lineEdit->text();
+	this->params.bitDepth = this->ui->spinBox_bitDepth->value();
+	this->params.width = this->ui->spinBox_width->value();
+	this->params.height = this->ui->spinBox_height->value();
+	this->params.depth = this->ui->spinBox_depth->value();
+	this->params.buffersPerVolume = this->ui->spinBox_buffersPerVolume->value();
+	this->params.buffersFromFile = this->ui->spinBox_buffersFromFile->value();
+	this->params.waitTimeUs = this->ui->spinBox_waitTime->value();
+	emit settingsUpdated(this->params);
+}
+
+void VirtualOCTSystemSettingsDialog::slot_enableGui(bool enable){
+	this->ui->lineEdit->setEnabled(enable);
+	this->ui->pushButton_selectFile->setEnabled(enable);
+	this->ui->spinBox_bitDepth->setEnabled(enable);
+	this->ui->spinBox_width->setEnabled(enable);
+	this->ui->spinBox_height->setEnabled(enable);
+	this->ui->spinBox_depth->setEnabled(enable);
+	this->ui->spinBox_buffersPerVolume->setEnabled(enable);
+	this->ui->spinBox_buffersFromFile->setEnabled(enable);
+	//this->ui->spinBox_waitTime->setEnabled(enable);  //waitTime does not need to be disabled. It can be safely changed during processing
+}
+
+void VirtualOCTSystemSettingsDialog::slot_checkWidthValue(){
+	int width = this->ui->spinBox_width->value();
+	if(width % 2 != 0){
+		this->ui->spinBox_width->setValue(width-1);
+	}
+}
