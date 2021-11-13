79065574197c95e98d7738c24a163fe0672aea1c
spectralcode <51879191+spectralcode@users.noreply.github.com>
2021-11-13 23:56:18 +0100

    guides updated

--- octproz_project/octproz/src/octproz.h
+++ octproz_project/octproz/src/octproz.h
@@ -55,8 +55,8 @@
 
 #include "ui_octproz.h"
 
-#define APP_VERSION "1.4.0"
-#define APP_VERSION_DATE "12 April 2021"
+#define APP_VERSION "1.4.1"
+#define APP_VERSION_DATE "13 November 2021"
 #define APP_NAME "OCTproZ"
 
 
