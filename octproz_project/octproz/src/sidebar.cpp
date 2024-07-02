faf0a94bc024a8dcd0cdb42e2592abe329361046
spectralcode <info@spectralcode.com>
2024-07-02 02:47:31 +0200

    bug in transfer dispersion compensation values fixed

--- octproz_project/octproz/src/sidebar.cpp
+++ octproz_project/octproz/src/sidebar.cpp
@@ -508,10 +508,10 @@ void Sidebar::slot_setDispCompCoeffs(double *d0, double *d1, double *d2, double 
 		this->ui.doubleSpinBox_d1->setValue(*d1);
 	}
 	if(d2 != nullptr){
-		this->ui.doubleSpinBox_c2->setValue(*d2);
+		this->ui.doubleSpinBox_d2->setValue(*d2);
 	}
 	if(d3 != nullptr){
-		this->ui.doubleSpinBox_c3->setValue(*d3);
+		this->ui.doubleSpinBox_d3->setValue(*d3);
 	}
 	QApplication::processEvents();
 	emit dispCompCoeffs(this->ui.doubleSpinBox_d0->value(), this->ui.doubleSpinBox_d1->value(), this->ui.doubleSpinBox_d2->value(), this->ui.doubleSpinBox_d3->value());
